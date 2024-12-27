19dd602540b58c488253b907c25a501650c33966
Prince Chaniyara <138362376+strPrince@users.noreply.github.com>
2024-12-27 19:00:11 +0530

    Update vite.config.js
    1.2

--- frontend/shareit/vite.config.js
+++ frontend/shareit/vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
+
+
 export default defineConfig({
-  base: '/shareit/',
-  plugins: [react()],
-})
+  base: '/frontend/shareit/', // Adjust this to the correct path where the app is hosted
+  build: {
+    outDir: 'build', // Ensure build outputs to 'build' folder
+  },
+});
